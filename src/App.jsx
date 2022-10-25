0791bf1d2305759742c6c74ecc0539407e1ad201
Luke Barker <luke.s.barker@gmail.com>
2022-10-25 10:34:52 -0500

    feat: create reducer
    begin work on reducer function (ready for 26 Oct class).

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "made_move": {
+    }
+  }
+}
 
 function App() {
-  // TODO: Add useReducer hook to track state of: board, turn, winner
+  const [state, dispatch] = useReducer(reducer, {
+    board: Array(9).fill(""),
+    turn: "X",
+    winner: null,
+  });
 
   return <h1>Tic Tac Toe</h1>;
 }
