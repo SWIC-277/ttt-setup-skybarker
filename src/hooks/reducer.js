4e6b859bdd6509f539cdd054e811a36d950df43a
Luke Barker <luke.s.barker@gmail.com>
2022-11-02 11:51:22 -0500

    Fix invalid move causing skip turn

--- src/hooks/reducer.js
+++ src/hooks/reducer.js
@@ -4,13 +4,15 @@ export default function reducer(state, action) {
   switch (action.type) {
     case "made_move": {
       const board2Update = [...state.board];
+      let currentTurn = state.turn;
       if (!board2Update[action.index]) {
         board2Update[action.index] = state.turn;
+        currentTurn = state.turn === "X" ? "O" : "X";
       }
 
       return {
         board: board2Update,
-        turn: state.turn === "X" ? "O" : "X",
+        turn: currentTurn,
         winner: check4Winner(board2Update, state.turn) ? state.turn : null,
       };
     }
